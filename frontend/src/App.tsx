6fdc5280e5f8934e2e70be03961eccc18cf512da
Michelle Madeira <michellemadeira1@gmail.com>
2022-07-15 21:07:34 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
- return (
-  <h1>Ola gente!!!!</h1>
- )
+  return (
+    <>
+      <h1>Ola gente!!!!</h1>
+      <NotificationButton />
+    </>
+  )
 }
+
 
 export default App
